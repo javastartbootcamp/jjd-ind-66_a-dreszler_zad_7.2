69e4621b6258499a965c0137cdad4a325febb1e2
Aleksander Dreszler <olek.d93@gmail.com>
2023-03-02 20:49:01 +0100

    Wykonano zadanie

--- src/main/java/pl/javastart/task/BubbleSort.java
+++ src/main/java/pl/javastart/task/BubbleSort.java
@@ -4,6 +4,30 @@ public class BubbleSort {
 
     // ta metoda powinna sortować inputArray oraz zwracać informację o liczbie porównań, które były do tego potrzebne
     public int sort(int[] inputArray) {
-        return 0;
+        int numbersToSort = inputArray.length;
+        int comparisons = 0;
+        while (numbersToSort > 1) {
+            int swapsInIteration = 0;
+            for (int i = 0; i < numbersToSort - 1; i++) {
+                if (inputArray[i] > inputArray[i + 1]) {
+                    swap(inputArray, i);
+                    swapsInIteration++;
+                }
+                comparisons++;
+            }
+            if (swapsInIteration == 0) {
+                break;
+            }
+            numbersToSort--;
+        }
+
+        return comparisons;
     }
+
+    private void swap(int[] array, int index) {
+        int temp = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = temp;
+    }
+
 }
